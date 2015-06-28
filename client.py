78c21fb03cf34fa6d2fa1a32a43986111580fad3
nplus <junnplus@qq.com>
2015-06-28 17:09:54 +0800

    msg

--- client.py
+++ client.py
@@ -2,4 +2,8 @@ import socket
 port = 8081
 host = 'localhost'
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.sendto("Holy Guido! It's working.", (host, port))
+BUFSIZE = 1024
+msg = raw_input()
+while msg:
+    bytes_sent = s.sendto(msg[:BUFSIZE], (host, port))
+    msg = msg[bytes_sent:]
